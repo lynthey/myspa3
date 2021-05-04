c70a266059e2362b07cd4c2c1eedbfb91ffdf067
lynthey <lynthey@gmail.com>
2021-05-04 21:40:04 +0900

    depricated port

--- app.py
+++ app.py
@@ -13,6 +13,4 @@ def print_string(string):
         return "Too few characters. Please type more than 20 characters."
 
 eel.init('web')
-eel.start('index.html', size=(600, 400), options={
-    'port': 8080
-})
+eel.start('index.html', size=(600, 400), port=8080)
